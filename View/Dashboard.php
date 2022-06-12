71007039c086747bbc1cc2344a0ff866df079c44
HarshanaEranda <harshawije1998@gmail.com>
2022-06-13 00:35:10 +0530

    add sidemenu

--- View/Dashboard.php
+++ View/Dashboard.php
@@ -6,14 +6,63 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <!--    CSS only-->
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="../bootstrap/css/Sidemenu.css">
+    <link rel="stylesheet" href="../bootstrap/css/custom.css">
     <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <!--Javascript only-->
-    <!--    <script src="../bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>-->
+        <script src="../bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-
-    <title>Login</title>
+    <script src="../bootstrap/js/NewJS/Sidemenu.js"></script>
+    <script src="https://kit.fontawesome.com/ec08fc2909.js" crossorigin="anonymous"></script>
+    <title>Dashboard</title>
 </head>
 <body>
+
+<div class="wrapper">
+    <!-- Sidebar -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Bootstrap Sidebar</h3>
+        </div>
+
+        <ul class="list-unstyled components">
+            <p>Dummy Heading</p>
+            <li>
+                <a href="#"><i class="fa-solid fa-user"></i> Profile</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa-solid fa-message"></i> Notifications</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa-solid fa-circle-question"></i> Help</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Page Content -->
+    <div id="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+
+                <button type="button" id="sidebarCollapse" class="btn btn-info">
+                    <i class="fas fa-align-left"></i>
+                </button>
+
+            </div>
+        </nav>
+    </div>
+
+</div>
 <div class="container">
     <div class="row">
         <div class="col-sm" >
@@ -22,14 +71,10 @@
         <div class="col-sm">
             One of three columns
         </div>
-        <div class="col-sm" >
-            One of three columns
-        </div>
-        <div class="col-sm" >
-            One of three columns
-        </div>
+
     </div>
 </div>
+
 
 </body>
 </html>
